53eb92b4ef3cef877c40b302360724c0eca04f5d
UP240088 <up240088@alumnos.upa.edu.mx>
2025-02-28 20:50:12 -0600

    Add files via upload

---
+++ Dia_2/01_Exercises.py
@@ -0,0 +1,34 @@
+#3
+first_name = 'Sebastian'
+#4
+last_name = 'Torres'
+#5
+ful_name = 'Jose Sebastian Torres Reyes'
+#6
+country = 'Mexico'
+#7
+city = 'Aguscalientes'
+#8
+age = 18
+#9
+year = 2025
+#10
+is_married = 'No'
+#11
+is_true = 'Si'
+#12
+is_light_on = 'No'
+#13
+A ,B ,C = "HOLA MUNDO","EJERCICIO 1 DIA 2","FIN"
+
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('ful_name:', ful_name)
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Year:', year)
+print('Married: ', is_married)
+print('Es verdad:', is_true)
+print('is_light_on:',is_light_on)
+print('Las variables son:', A ,B ,C)
---
+++ Dia_2/02_Exercises.py
@@ -0,0 +1,56 @@
+first_name = 'Sebastian'
+last_name = 'Torres'
+#2
+print('First name:', first_name)
+print('First name length:', len(first_name))
+#3
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+#4
+num_one = 5
+num_two = 4
+#5
+Total = num_one + num_two
+#6
+diff = num_two - num_one
+#7
+product = num_two * num_one
+#8
+division = num_one / num_two
+#9
+remainder = num_one % num_two
+#10
+exp = num_one ** num_two
+#11
+floor_division = num_one // num_two
+
+print('Total:',num_one + num_two)
+print('Diff:',num_two - num_one)
+print('Producto:',num_two * num_one)
+print('Division:',num_one / num_two)
+print('Remainder:',num_one % num_two)
+print('Exp:',num_one ** num_two)
+print('Floor_Division:',num_one // num_two)
+
+#12
+     #1
+radius = 30
+area_of_circle = 3.1416 * radius **2
+print('El area es:',area_of_circle) 
+      #2
+circum_of_circle = 3.1416 * radius
+print('La circunferencia es:',circum_of_circle)
+      #3
+radius=float(input("Introduce el radio del circulo:"))
+area= 3.1416 * radius ** 2
+print("El area del circulo es:", area)
+#13
+nombres = input("Introduce tus nombres: ")
+apellidos = input("Introduce tus apellidos: ")
+pais = input("Introduce tu país: ")
+edad = input("Introduce tu edad: ")
+
+print("Hola",nombres,apellidos, "De" , pais ,"  " "Tienes", edad , "años")
+
+#14
+help('keywords')
