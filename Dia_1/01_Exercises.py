14a96d68107a735ef640936592bd1cc6a52e8434
UP240088 <up240088@alumnos.upa.edu.mx>
2025-02-28 20:49:31 -0600

    Add files via upload

---
+++ Dia_1/01_Exercises.py
@@ -0,0 +1,21 @@
+#SUMA(+)
+print( 3+4)
+#RESTA(-)
+print( 3-4 )
+#MULTIPLICACION(*)
+print( 3*4 )
+#PORSENTAJE(%)
+print(3%4)
+#DIVISION(/)
+print(3/4)
+#EXPONENCIAL(**)
+print(3**4)
+#FLOOR DIVISION OPERATOR(//)
+print(3//4)
+
+#MY NAME
+print("My name is Sebastian")
+#YOUR FAMILY NAME
+print("My famili names is Sarai,Ismael,Jimena,Rafael,Sebastian")
+#YOUR COUNTRY
+print("IM ENJOYING 30 DAYS OF PYTHON")
---
+++ Dia_1/02_Exercises.py
@@ -0,0 +1,21 @@
+#  Int
+print(type(9))
+#  Float
+print(type(3.14))
+#  Complex
+print(type(4-4j))
+#  String
+print(type("Asabeneh"))
+# List
+print(type([1,2,3,4]))
+# Dictionary
+print(type({'name':'Asabeneh'}))
+# Set
+print(type({9.8, 3.14, 2.7}))
+# Tuple
+print(type((9.8, 3.14, 2.7)))
+# Bool
+print(type(3 == 3)) 
+# Bool
+print(type(3 >= 3))
+
